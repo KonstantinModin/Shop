2b7db5eeba256188d816106faf400cfb6c1ba653
Konstantin <konstantin.v.modin@gmail.com>
2019-06-27 06:46:23 +0200

    updates

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,31 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary';
+import ContactData from './ContactData';
+import { Route } from 'react-router-dom';
 import './Checkout.css';
 
-const Checkout = (props) => {
-    // const [ingredients] = useState({
-    //     salad: 1,
-    //     meat: 1,
-    //     cheese: 1,
-    //     bacon: 1
-    // });
+const Checkout = (props) => {    
+
+    const ingredients = Object.assign({}, props.location.state);
 
     const checkoutCancelledHandler = () => {
         props.history.goBack();
     }
 
     const checkoutContinuedHandler = () => {
-        props.history.replace('/checkout/contact-data');
-    }
-
-    console.log('props.location.state :', props.location.state);
+        props.history.replace('/checkout/contact-data', ingredients);
+    } 
+       
+    useEffect(() => {
+        console.log('ingredients :', ingredients);
+        console.log('props.location.state', props.location.state);
+    });
 
     return (
         <div className="Checkout">
             <CheckoutSummary 
-                ingredients={props.location.state}
+                ingredients={ingredients}
                 checkoutContinued={checkoutContinuedHandler}
                 checkoutCancelled={checkoutCancelledHandler} />
+            <Route path={props.match.path + '/contact-data'} component={ContactData} />
         </div>
     )
 }
