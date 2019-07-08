cb7e66da08ddd679f9220046ad7221c1dbb27805
Konstantin <konstantin.v.modin@gmail.com>
2019-07-08 05:54:53 +0200

    updates

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -41,8 +41,47 @@ export default class Auth extends Component {
         },
         formIsValid: false
     }
+
+    checkValidity({validation, value, shouldValidate}) {        
+        if (!shouldValidate) return true;
+        let isValid = true;
+        if (validation.required) {
+            isValid = value.trim() !== '' && isValid;            
+        }
+        
+        if (validation.minLength) {
+            isValid = value.length >= validation.minLength && isValid;            
+        }
+        if (validation.maxLength) {
+            isValid = value.length <= validation.maxLength && isValid;            
+        }
+        
+        if (validation.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid;           
+        }
+        return isValid;
+    }
+
+    inputChangedHandler = (event, controlName) => {       
+        const updatedControls = {...this.state.controls};
+        const updatedItem = {...updatedControls[controlName]};
+        updatedItem.value = event.target.value;
+        updatedItem.valid = this.checkValidity(updatedItem);
+        updatedItem.touched = true;        
+        updatedControls[controlName] = updatedItem;
+        this.setState({controls: updatedControls});
+    }
+
+    componentDidUpdate(_, prevState){
+        const formIsValid = Object.entries(this.state.controls).every(i => i[1].valid);        
+        if (formIsValid !== prevState.formIsValid) {
+            this.setState({formIsValid: formIsValid});
+        };        
+    };
     
     render() {
+        console.log('this.state.formIsValid :', this.state.formIsValid);
         return (
             <div className="Auth">
                 <form>
