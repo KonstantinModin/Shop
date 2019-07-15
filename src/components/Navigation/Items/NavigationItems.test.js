8f9da3ecd4bfad8c482485b4393f45f052ecb4f3
Konstantin <konstantin.v.modin@gmail.com>
2019-07-15 06:58:23 +0200

    updates

--- src/components/Navigation/Items/NavigationItems.test.js
+++ src/components/Navigation/Items/NavigationItems.test.js
@@ -10,8 +10,35 @@ import { NavLink } from 'react-router-dom';
 configure({adapter: new Adapter()});
 
 describe('<Navigation Items />', () => {
-    it('should render 2 <Navigation Items /> elements if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems />);
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    });
+    
+    it('should render 2 <Navigation Items /> elements if not authenticated', () => {       
         expect(wrapper.find(NavLink)).toHaveLength(2);
     });
+
+    it('should render 3 <Navigation Items /> elements if authenticated', () => {
+        wrapper.setProps({isAuth: true});
+        expect(wrapper.find(NavLink)).toHaveLength(3);
+    });
+    
+    it('should render 3 <Navigation Items /> elements if authenticated', () => {
+        wrapper.setProps({isAuth: true});
+        expect(wrapper.find(NavLink)).toHaveLength(3);
+    });
+
 });
+
+// describe('<Navigation Items />', () => {
+//     it('should render 2 <Navigation Items /> elements if not authenticated', () => {
+//         const wrapper = shallow(<NavigationItems />);
+//         expect(wrapper.find(NavLink)).toHaveLength(2);
+//     });
+//     it('should render 3 <Navigation Items /> elements if authenticated', () => {
+//         const wrapper = shallow(<NavigationItems isAuth />);
+//         expect(wrapper.find(NavLink)).toHaveLength(3);
+//     });
+// });
