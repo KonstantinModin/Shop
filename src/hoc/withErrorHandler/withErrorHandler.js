27a4c8c4260905a4b6580558546c06bfdfe1d698
Konstantin <konstantin.v.modin@gmail.com>
2019-07-29 06:19:21 +0200

    updates

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,38 +1,40 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import Modal from '../../components/UI/Modal';
+import useHttpErrorHandler from '../../hooks/http-error-handler';
 
 const withErrorHandler = (WrappedComponent, axios) => {
     return (props) => {
-        const [errorState, setErrorState] = useState(null);
+        // const [errorState, setErrorState] = useState(null);
         
-        const reqInterseptor = axios.interceptors.request.use(req => {
-            setErrorState(null);
-            return req;
-        });
-        const responseInterseptor = axios.interceptors.response.use(res => res, error => {               
-            setErrorState(error);                
-        });
+        // const reqInterseptor = axios.interceptors.request.use(req => {
+        //     setErrorState(null);
+        //     return req;
+        // });
+        // const responseInterseptor = axios.interceptors.response.use(res => res, error => {               
+        //     setErrorState(error);                
+        // });
         
 
-        useEffect(() => {
-            return () => {
-                axios.interceptors.request.eject(reqInterseptor);
-                axios.interceptors.response.eject(responseInterseptor);  
-                // console.log('will unmount interseptors!', reqInterseptor, responseInterseptor);
-            }
-        }, [reqInterseptor, responseInterseptor]);
+        // useEffect(() => {
+        //     return () => {
+        //         axios.interceptors.request.eject(reqInterseptor);
+        //         axios.interceptors.response.eject(responseInterseptor);  
+        //         // console.log('will unmount interseptors!', reqInterseptor, responseInterseptor);
+        //     }
+        // }, [reqInterseptor, responseInterseptor]);
+        const [error, clearError] = useHttpErrorHandler(axios);
 
                    
         return (
             <>
                 {useMemo(()=>(
                     <Modal 
-                    show={errorState}
-                    modalClosed={() => setErrorState(null)}>
+                    show={error}
+                    modalClosed={clearError}>
                     <h1>We've got an error here:</h1>
-                    <h2>{errorState ? errorState.message : null}</h2>
+                    <h2>{error ? error.message : null}</h2>
                     </Modal>
-                ), [errorState])}                
+                ), [error, clearError])}                
                 <WrappedComponent {...props}/>
             </>
         )
