c3f4a1fa316f1294c245dd4bd2e63f2f8ffdc97d
Konstantin <konstantin.v.modin@gmail.com>
2019-12-23 21:14:44 +0100

    updates

--- src/components/Burger/Ingredient/Ingredient.js
+++ src/components/Burger/Ingredient/Ingredient.js
@@ -16,25 +16,3 @@ Ingredient.propTypes = {
 }
 
 export default Ingredient;
-
-    // switch (props.type) {
-    //     case 'bread-bottom': ingredient = <div className="BreadBottom"></div>;
-    //         break;
-    //     case 'bread-top': ingredient = (
-    //         <div className="BreadTop">
-    //             <div className="Seeds1"></div>
-    //             <div className="Seeds2"></div>
-    //         </div>
-    //     );
-    //         break;
-    //     case 'meat': ingredient = <div className="Meat"></div>;
-    //         break;
-    //     case 'cheese': ingredient = <div className="Cheese"></div>;
-    //         break;
-    //     case 'salad': ingredient = <div className="Salad"></div>;
-    //         break;
-    //     case 'bacon': ingredient = <div className="Bacon"></div>;
-    //         break;
-    //     default: ingredient = null;
-    // return ingredient;
-    // }
