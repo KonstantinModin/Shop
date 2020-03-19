59947a5ab4c559faee04f2dfae6cbecf07692a0d
KonstantinModin <konstantin.v.modin@gmail.com>
2020-03-19 16:06:46 +0100

    updates

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,28 +1,40 @@
-import { AUTH_START, AUTH_SUCCESS, AUTH_FAIL, AUTH_LOGOUT, SET_AUTH_REDIRECT_PATH } from '../actions/actionTypes';
-import { updateObject } from '../utility';
+import {
+    AUTH_START,
+    AUTH_SUCCESS,
+    AUTH_FAIL,
+    AUTH_LOGOUT,
+    SET_AUTH_REDIRECT_PATH
+} from "../actions/actionTypes";
+import { updateObject } from "../utility";
 
 const initialState = {
     token: null,
     userId: null,
     error: null,
     loading: false,
-    authRedirectPath: '/'
+    authRedirectPath: "/"
 };
 
 const reducer = (state = initialState, { type, payload }) => {
     switch (type) {
-        case AUTH_START: return updateObject(state, {error: null, loading: true});
-        case AUTH_FAIL: return updateObject(state, {error: payload, loading: false});
-        case AUTH_SUCCESS: return updateObject(state, {
-            token: payload.idToken,
-            userId: payload.userId,
-            error: null, 
-            loading: false
-        });
-        case AUTH_LOGOUT: return initialState;
-        case SET_AUTH_REDIRECT_PATH: return updateObject(state, {authRedirectPath: payload});
-        default: return state;
-    };
+        case AUTH_START:
+            return updateObject(state, { error: null, loading: true });
+        case AUTH_FAIL:
+            return updateObject(state, { error: payload, loading: false });
+        case AUTH_SUCCESS:
+            return updateObject(state, {
+                token: payload.idToken,
+                userId: payload.userId,
+                error: null,
+                loading: false
+            });
+        case AUTH_LOGOUT:
+            return initialState;
+        case SET_AUTH_REDIRECT_PATH:
+            return updateObject(state, { authRedirectPath: payload });
+        default:
+            return state;
+    }
 };
 
 export default reducer;
--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,9 +1,9 @@
-import { 
-    HANDLE_INGREDIENT,      
-    SET_INGREDIENTS, 
+import {
+    HANDLE_INGREDIENT,
+    SET_INGREDIENTS,
     FETCH_INGREDIENTS_FAILED,
-    INIT_INGREDIENT 
-} from '../actions/actionTypes';
+    INIT_INGREDIENT
+} from "../actions/actionTypes";
 
 import { updateObject } from "../utility";
 
@@ -21,32 +21,40 @@ const initialState = {
     building: false
 };
 
-const reducer = (state = initialState, {type, payload}) => {
-    switch(type) {
+const reducer = (state = initialState, { type, payload }) => {
+    switch (type) {
         case INIT_INGREDIENT: {
-            // console.log('INIT_INGREDIENT Reducer');
             return state;
         }
-        case SET_INGREDIENTS: return updateObject(state,
-            {ingredients: {
-                salad: payload.salad,
-                bacon: payload.bacon,
-                cheese: payload.cheese,
-                meat: payload.meat
-            },
-            totalPrice: 2,
-            error: false,
-            building: false});
-            
-        case FETCH_INGREDIENTS_FAILED: return updateObject(state, {error: true});
+        case SET_INGREDIENTS:
+            return updateObject(state, {
+                ingredients: {
+                    salad: payload.salad,
+                    bacon: payload.bacon,
+                    cheese: payload.cheese,
+                    meat: payload.meat
+                },
+                totalPrice: 2,
+                error: false,
+                building: false
+            });
 
-        case HANDLE_INGREDIENT: return updateObject(state,
-            {ingredients: updateObject(state.ingredients,
-                {[payload.name]: state.ingredients[payload.name] + payload.q}),
-            totalPrice: state.totalPrice + INGREDIENT_PRICES[payload.name] * payload.q,
-            building: true});        
-        
-        default: return state;
+        case FETCH_INGREDIENTS_FAILED:
+            return updateObject(state, { error: true });
+
+        case HANDLE_INGREDIENT:
+            return updateObject(state, {
+                ingredients: updateObject(state.ingredients, {
+                    [payload.name]: state.ingredients[payload.name] + payload.q
+                }),
+                totalPrice:
+                    state.totalPrice +
+                    INGREDIENT_PRICES[payload.name] * payload.q,
+                building: true
+            });
+
+        default:
+            return state;
     }
 };
 
