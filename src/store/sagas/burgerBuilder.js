e49180dcc4b015cb504d0caa2f9d7212c4decaff
KonstantinModin <konstantin.v.modin@gmail.com>
2020-03-20 07:47:07 +0100

    updates

--- src/store/sagas/burgerBuilder.js
+++ src/store/sagas/burgerBuilder.js
@@ -1,12 +1,14 @@
-import { put } from 'redux-saga/effects';
-import server from '../../axios-orders';
-import {setIngredients, fetchIngredientsFailed } from '../actions';
+import { put } from "redux-saga/effects";
+import server from "../../axios-orders";
+import { setIngredients, fetchIngredientsFailed } from "../actions";
 
 export function* initIngredientSaga(action) {
     try {
-        const response = yield server.get('https://burger-shop-6267.firebaseio.com/ingredients.json');
-        yield put(setIngredients(response.data));        
-    } catch(error) {
+        const response = yield server.get(
+            "https://burger-shop-6267.firebaseio.com/ingredients.json"
+        );
+        yield put(setIngredients(response.data));
+    } catch (error) {
         yield put(fetchIngredientsFailed(error));
     }
-};
+}
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,21 +1,33 @@
-import { takeEvery } from 'redux-saga/effects';
-import { AUTH_INITIATE_LOGOUT, AUTH_CHECK_TIMEOUT, AUTH_USER, 
-    AUTH_CHECK_INITIAL_STATE, INIT_INGREDIENT, FETCH_PURCHASE_BURGER, FETCH_ORDERS_INIT } from '../actions/actionTypes';
+import { takeEvery } from "redux-saga/effects";
+import {
+    AUTH_INITIATE_LOGOUT,
+    AUTH_CHECK_TIMEOUT,
+    AUTH_USER,
+    AUTH_CHECK_INITIAL_STATE,
+    INIT_INGREDIENT,
+    FETCH_PURCHASE_BURGER,
+    FETCH_ORDERS_INIT
+} from "../actions/actionTypes";
 
-import { logoutSaga, checkAuthTimeoutSaga, authSaga, authCheckStateSaga } from './auth';
-import { initIngredientSaga } from './burgerBuilder';
-import { purchaseBurgerSaga, fetchOrdersSaga } from './order';
+import {
+    logoutSaga,
+    checkAuthTimeoutSaga,
+    authSaga,
+    authCheckStateSaga
+} from "./auth";
+import { initIngredientSaga } from "./burgerBuilder";
+import { purchaseBurgerSaga, fetchOrdersSaga } from "./order";
 
 export function* watchAuth() {
     yield takeEvery(AUTH_INITIATE_LOGOUT, logoutSaga);
     yield takeEvery(AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
     yield takeEvery(AUTH_USER, authSaga);
     yield takeEvery(AUTH_CHECK_INITIAL_STATE, authCheckStateSaga);
-};
+}
 
 export function* watchBurgerBuilder() {
-    yield takeEvery(INIT_INGREDIENT, initIngredientSaga);    
-};
+    yield takeEvery(INIT_INGREDIENT, initIngredientSaga);
+}
 
 export function* watchOrder() {
     yield takeEvery(FETCH_PURCHASE_BURGER, purchaseBurgerSaga);
