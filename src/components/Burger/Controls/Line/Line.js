d15a00cf4a708b118709284de8fc35e0988f18ab
KonstantinModin <konstantin.v.modin@gmail.com>
2020-03-25 20:07:39 +0100

    updates

--- src/components/Burger/Controls/Line/Line.js
+++ src/components/Burger/Controls/Line/Line.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import './Line.css';
+import React from "react";
+import "./Line.css";
 
-const Line = ({ label, disabled, action, type}) => {
+const Line = ({ label, disabled, action, type }) => {
     return (
         <div className="Line">
             <div className="Label">{label}</div>
-            <button className="Less" onClick={() => action(type, -1)} disabled={disabled}>Less</button>   
-            <button className="More" onClick={() => action(type, 1)}>More</button>   
+            <button
+                className="Less"
+                onClick={() => action(type, -1)}
+                disabled={disabled}
+            >
+                Less
+            </button>
+            <button className="More" onClick={() => action(type, 1)}>
+                More
+            </button>
         </div>
-    )
-}
+    );
+};
 
 export default Line;
