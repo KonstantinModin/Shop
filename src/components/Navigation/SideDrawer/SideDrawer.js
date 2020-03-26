c28597b356aa6adb34d1abfe8ff0d806a4c1a9b8
KonstantinModin <konstantin.v.modin@gmail.com>
2020-03-26 19:00:11 +0100

    updates

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import './SideDrawer.css';
-import Logo from '../../Logo/';
-import NavigationItems from '../Items';
-import Backdrop from '../../UI/Backdrop';
+import React from "react";
+import "./SideDrawer.css";
+import Logo from "../../Logo/";
+import NavigationItems from "../Items";
+import Backdrop from "../../UI/Backdrop";
 
-const SideDrawer = (props) => {
-    const classes = ['SideDrawer'].concat(props.open ? 'Open' : 'Close').join` `;
+const SideDrawer = props => {
+    const classes = ["SideDrawer"].concat(props.open ? "Open" : "Close")
+        .join` `;
     return (
         <>
-            <Backdrop show={props.open} clicked={props.closed}/>
+            <Backdrop show={props.open} clicked={props.closed} />
             <div className={classes} onClick={props.closed}>
-                <Logo st={{height: '11%'}}/>
+                <Logo st={{ height: "11%" }} />
                 <nav>
-                    <NavigationItems isAuth={props.isAuth}/>
+                    <NavigationItems isAuth={props.isAuth} />
                 </nav>
             </div>
         </>
-    )
+    );
 };
 
 export default SideDrawer;
