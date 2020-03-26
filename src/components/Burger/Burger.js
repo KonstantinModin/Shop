05f9ad8fda8aff37e1ac17e547dfbe8cdde76b2d
KonstantinModin <konstantin.v.modin@gmail.com>
2020-03-26 16:40:15 +0100

    updates

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import Ingredient from './Ingredient';
-import './Burger.css';
+import React from "react";
+import Ingredient from "./Ingredient";
+import "./Burger.css";
 
-const Burger = ({ingredients}) => {
+const Burger = ({ ingredients }) => {
     let transformedIngredients = Object.keys(ingredients)
-        .map(igKey => [...Array(ingredients[igKey])]
-        .map((_, i) => <Ingredient key={igKey + i} type={igKey} />))
+        .map(igKey =>
+            [...Array(ingredients[igKey])].map((_, i) => (
+                <Ingredient key={igKey + i} type={igKey} />
+            ))
+        )
         .reduce((a, b) => [...a, ...b], []);
-    
-    
-    if (transformedIngredients.length === 0) 
-    transformedIngredients = <p>Please start adding ingredients!</p>;
-    
+
+    if (transformedIngredients.length === 0)
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+
     return (
-        <div className="Burger">            
-            <Ingredient type="bread-top"/>
+        <div className="Burger">
+            <Ingredient type="bread-top" />
             {transformedIngredients}
-            <Ingredient type="bread-bottom"/>
+            <Ingredient type="bread-bottom" />
         </div>
-    )    
-}
+    );
+};
 
 export default Burger;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,28 +1,29 @@
-import React, { useState } from 'react';
-import { connect } from 'react-redux';
-import './Layout.css';
-import Toolbar from '../Navigation/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import "./Layout.css";
+import Toolbar from "../Navigation/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/";
 
 const Layout = ({ isAuth, children }) => {
-    const [ showSideDrawer, setShowSideDrawer ] = useState(false);
-    
-    const sideDrawerHandler = (value) => setShowSideDrawer(value);    
-    
-    return  (
-        <>            
-            <Toolbar isAuth={isAuth} openSideDraw={() => sideDrawerHandler(true)}/>
-            <SideDrawer
-                isAuth={isAuth} 
-                open={showSideDrawer} 
-                closed={() => sideDrawerHandler(false)}
-            /> 
+    const [showSideDrawer, setShowSideDrawer] = useState(false);
 
-            <main className="LayoutContent">
-                {children}                
-            </main>
+    const sideDrawerHandler = value => setShowSideDrawer(value);
+
+    return (
+        <>
+            <Toolbar
+                isAuth={isAuth}
+                openSideDraw={() => sideDrawerHandler(true)}
+            />
+            <SideDrawer
+                isAuth={isAuth}
+                open={showSideDrawer}
+                closed={() => sideDrawerHandler(false)}
+            />
+
+            <main className="LayoutContent">{children}</main>
         </>
-    )    
+    );
 };
 
 const mapStateToProps = state => {
