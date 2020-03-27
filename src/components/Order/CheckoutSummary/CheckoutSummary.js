e05b105da55a9b7a50023609ca6b42bd4e88b7ab
KonstantinModin <konstantin.v.modin@gmail.com>
2020-03-27 16:47:41 +0100

    updates

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import './CheckoutSummary.css';
-import Burger from '../../Burger';
-import Button from '../../UI/Button';
+import React from "react";
+import "./CheckoutSummary.css";
+import Burger from "../../Burger";
+import Button from "../../UI/Button";
 
-const CheckoutSummary = (props) => {
+const CheckoutSummary = props => {
     return (
         <div className="CheckoutSummary">
             <h1>We hope it tastes well!</h1>
             <div className="Burger">
-                <Burger ingredients={props.ingredients}/>
+                <Burger ingredients={props.ingredients} />
             </div>
             <h2>The total price of your order is: {props.price.toFixed(2)}$</h2>
-            <Button btnType="Danger" clicked={props.checkoutCancelled}>Cancel</Button>
-            <Button btnType="Success" clicked={props.checkoutContinued}>Continue</Button>
+            <Button btnType="Danger" clicked={props.checkoutCancelled}>
+                Cancel
+            </Button>
+            <Button btnType="Success" clicked={props.checkoutContinued}>
+                Continue
+            </Button>
         </div>
-    )
+    );
 };
 
 export default CheckoutSummary;
--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import './Order.css';
+import React from "react";
+import "./Order.css";
 
-const Order = ({ingredients, price, date}) => {
+const Order = ({ ingredients, price, date }) => {
     // console.log(date);
-    
+
     // const myDate = [date.getDate() + date.getMonth(), date.getTime()];
     // const myDate = date.split`T`;
-    const myDate = (new Date(date)).toUTCString();
+    const myDate = new Date(date).toUTCString();
     // console.log(myDate);
-
 
     const ingr = Object.entries(ingredients)
         .filter(i => i[1] > 0)
-        .map(([a, b]) => <span key={a}> {a} ( {b} )</span>);    
-    
+        .map(([a, b]) => (
+            <span key={a}>
+                {" "}
+                {a} ( {b} )
+            </span>
+        ));
+
     return (
         <div className="Order">
-            <h3>Date of order: {myDate.slice(4, 16)},  Time of order: {myDate.slice(17, 25)}</h3>
+            <h3>
+                Date of order: {myDate.slice(4, 16)}, Time of order:{" "}
+                {myDate.slice(17, 25)}
+            </h3>
             <h3>Ingredients: {ingr} </h3>
-            <h3>Price: <strong>{price}$</strong></h3>
+            <h3>
+                Price: <strong>{price}$</strong>
+            </h3>
         </div>
-    )
+    );
 };
 
 export default Order;
